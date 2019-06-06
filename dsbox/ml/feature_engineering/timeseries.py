c7c76c044be3a4e65fc418768deb39c5281789d7
VLE <vle@octo.com>
2019-06-06 11:55:48 +0200

    add create_diff_shift_features for TS

--- dsbox/ml/feature_engineering/timeseries.py
+++ dsbox/ml/feature_engineering/timeseries.py
@@ -375,3 +375,21 @@ def transform_datetxt2int(X, col_date, format='%Y-%m-%d'):
     X[col_date] = pd.to_datetime(X[col_date], format=format)
     X[col_date] = X[col_date].map(lambda x: (x.year * 10 ** 4) + (x.month * 10 ** 2) + x.day)
     X[col_date] = X[col_date].astype('int')
+
+
+def create_diff_shift_features(df_shift, cols=[], prefix='diff_'):
+    """
+    Create diff values between time series columns with have been shifted.
+
+    Parameters
+    ----------
+    df_shift: dataframe
+        Dataset with columns to make apply diff.
+    cols: list
+        Columns names, in order, to apply diff.
+    prefix: str
+        Prefix used to name diff columns.
+
+    """
+    for i in range(0, len(cols) - 1):
+        df_shift[prefix + cols[i + 1] + '_' + cols[i]] = df_shift[cols[i + 1]] - df_shift[cols[i]]
