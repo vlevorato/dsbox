e5b3a1ebcae793582c93adb627850ae149ce91c1
vlevorat <vlevorat@prismamedia.com>
2022-11-28 10:17:28 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ds-box",
-    version='1.4.2',
+    version='1.4.3',
     author='Vincent Levorato',
     url='https://github.com/vlevorato/dsbox',
     description="Package made to accelerate data science workflows in production.",
